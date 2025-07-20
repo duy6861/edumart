d15017e4027cb081267bc7013c3ce38151602a62
duy6861 <duy6861@gmail.com>
2025-07-20 09:36:38 +0700

    update-modal

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require('tailwindcss/defaultTheme');
-
-module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {
-      screens: {
-        // Raw orientation breakpoints
-        'portrait': { raw: '(orientation: portrait)' },
-        'landscape': { raw: '(orientation: landscape)' },
-
-        'landscape-phone': {
-          raw: '(orientation: landscape) and (max-width: 640px)'
-        },
-        'portrait-phone': {
-          raw: '(orientation: portrait) and (max-width: 400px)'
-        },
-      },
-      screens: {
-        //  breakpoint
-        sm: '640px',
-        md: '768px',
-        lg: '1024px',
-        xl: '1280px',
-        '2xl': '1536px',
-      },
-    },
+    extend: {},
   },
   plugins: [],
-};
+}
+
